e742cf81e7cfcd2a1e547f44547913bfd79e46c1
sayudhalw <mazaya.studio@outlook.com>
2023-12-11 22:57:49 +0700

    Fetching data with javascript

---
+++ script.js
@@ -0,0 +1,23 @@
+let apiQuotes = [];
+
+// Show new quote
+function newQuote() {
+  // Pick a random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
+}
+
+// Get quotes from API
+async function getQuotes() {
+  const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    // catch error
+  }
+}
+
+// on load
+getQuotes();
